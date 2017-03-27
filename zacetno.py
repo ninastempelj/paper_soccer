c7c3c082b888488d35b52f4f6bd7623920e4635a
Urša Pertot <ursa.pertot@student.fmf.uni-lj.si>
2017-03-27 12:21:12 +0200

    začetno 2.del

--- zacetno.py
+++ zacetno.py
@@ -4,8 +4,11 @@ class Zacetno:
     def __init__(self, master):
 ##        self.igralec1 = None
 ##        self.igralec2 = None
-                
-    #Spremenljivke na začetnem polju
+
+        self.dovoljene_barve = ['yellow', 'blue']
+        self.barva_igralec1 = 'red'
+        self.barva_igralec2 = 'green'
+        
         self.napis_gumb1 = tk.StringVar()
         self.napis_gumb1.set("Čarovnik")
         self.napis_gumb2 = tk.StringVar()
@@ -22,29 +25,69 @@ class Zacetno:
                           command=self.spremeni_igralca2)
         gumb2.grid(row=1, column=5)
 
+        #Gumbi za 1.igralca:
+        barve1 = tk.Label(master, text = "Izberi dom:")
+        barve1.grid(row=2, column=0)
+        G1 = tk.Button(master, text = "Gryfondom",
+                       command = lambda: self.izberi_barvo1('red',G1))
+        G1.grid(row=3, column=0)
+
+        P1 = tk.Button(master, text = "Pihpuff",
+                       command = lambda: self.izberi_barvo1('yellow',P1))
+        P1.grid(row=3, column=1)
+
+        D1 = tk.Button(master, text = "Drznvraan",
+                       command = lambda: self.izberi_barvo1('blue',D1))
+        D1.grid(row=4, column=0)
+
+        S1 = tk.Button(master, text = "Spolzgad",
+                       command = lambda: self.izberi_barvo1('green',S1))
+        S1.grid(row=4, column=1)
+
+        self.gumbi_igralca1 = [G1, P1, D1, S1]
+
+    #Gumbi za 2.igralca:
+        barve2 = tk.Label(master, text = "Izberi dom:")
+        barve2.grid(row=2, column=5)
+        G2 = tk.Button(master, text = "Gryfondom",
+                       command = lambda: self.izberi_barvo1('red', G2))
+        G2.grid(row=3, column=5)
+
+        P2 = tk.Button(master, text = "Pihpuff",
+                       command = lambda: self.izberi_barvo1('yellow',P2))
+        P2.grid(row=3, column=6)
+
+        D2 = tk.Button(master, text = "Drznvraan",
+                       command = lambda: self.izberi_barvo1('blue',D2))
+        D2.grid(row=4, column=5)
+
+        S2 = tk.Button(master, text = "Spolzgad",
+                       command = lambda: self.izberi_barvo1('green',S2))
+        S2.grid(row=4, column=6)
+
+        self.gumbi_igralca2 = [G2, P2, D2, S2]
+
     def spremeni_igralca1(self, master):
-        if True:
-            barve1 = tk.Label(master, text = "Izberi dom:")
-            barve1.grid(row=2, column=0)
-            G1 = tk.Button(master, text = "Gryfondom",
-                           command = lambda: self.izberi_barvo1(G))
-            G1.grid(row=3, column=0)
-
-            P1 = tk.Button(master, text = "Pihpuff",
-                           command = lambda: self.izberi_barvo1(P))
-            P1.grid(row=3, column=1)
-
-            D1 = tk.Button(master, text = "Drznvraan",
-                           command = lambda: self.izberi_barvo1(D))
-            D1.grid(row=4, column=0)
-
-            S1 = tk.Button(master, text = "Spolzgad",
-                           command = lambda: self.izberi_barvo1(S))
-            S1.grid(row=4, column=1)
+        if True: #Pove ali izbran čarovnik ali duh ampak še nimamo te spremenljivke
+            pass
             
 
-    def izberi_barvo1(self):
-        pass
+    def izberi_barvo1(self, barva, gumb):
+        if barva in self.dovoljene_barve:
+            self.dovoljene_barve.remove(barva)
+            if gumb in self.gumbi_igralca1:
+                self.dovoljene_barve.append(self.barva_igralec1)
+                self.barva_igralec1 = barva
+            if gumb in self.gumbi_igralca2:
+                self.dovoljene_barve.append(self.barva_igralec2)
+                self.barva_igralec2 = barva
+        else:
+            pass
+
+
+        print(self.barva_igralec1,self.barva_igralec2, gumb)
+        
+        
     
     def spremeni_igralca2(self):
         pass
