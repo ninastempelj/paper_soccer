cdc27101455d86296a546fb20e13052f5a353ee1
Urša Pertot <ursa.pertot@student.fmf.uni-lj.si>
2017-03-27 12:44:23 +0200

    Zacetno2

--- zacetno.py
+++ zacetno.py
@@ -33,20 +33,20 @@ class Zacetno:
         #Gumbi za 1.igralca:
         barve1 = tk.Label(master, text = "Izberi dom:")
         barve1.grid(row=2, column=0)
-        G1 = tk.Button(master, text = "Gryfondom",
-                       command = lambda: self.izberi_barvo1('red',G1))
+        G1 = tk.Button(master, text = "Gryfondom", relief='sunken',
+                       command = lambda: self.izberi_barvo('red',G1))
         G1.grid(row=3, column=0)
 
         P1 = tk.Button(master, text = "Pihpuff",
-                       command = lambda: self.izberi_barvo1('yellow',P1))
+                       command = lambda: self.izberi_barvo('yellow',P1))
         P1.grid(row=3, column=1)
 
         D1 = tk.Button(master, text = "Drznvraan",
-                       command = lambda: self.izberi_barvo1('blue',D1))
+                       command = lambda: self.izberi_barvo('blue',D1))
         D1.grid(row=4, column=0)
 
         S1 = tk.Button(master, text = "Spolzgad",
-                       command = lambda: self.izberi_barvo1('green',S1))
+                       command = lambda: self.izberi_barvo('green',S1))
         S1.grid(row=4, column=1)
 
         self.gumbi_igralca1 = [G1, P1, D1, S1]
@@ -55,19 +55,19 @@ class Zacetno:
         barve2 = tk.Label(master, text = "Izberi dom:")
         barve2.grid(row=2, column=5)
         G2 = tk.Button(master, text = "Gryfondom",
-                       command = lambda: self.izberi_barvo1('red', G2))
+                       command = lambda: self.izberi_barvo('red', G2))
         G2.grid(row=3, column=5)
 
         P2 = tk.Button(master, text = "Pihpuff",
-                       command = lambda: self.izberi_barvo1('yellow',P2))
+                       command = lambda: self.izberi_barvo('yellow',P2))
         P2.grid(row=3, column=6)
 
         D2 = tk.Button(master, text = "Drznvraan",
-                       command = lambda: self.izberi_barvo1('blue',D2))
+                       command = lambda: self.izberi_barvo('blue',D2))
         D2.grid(row=4, column=5)
 
-        S2 = tk.Button(master, text = "Spolzgad",
-                       command = lambda: self.izberi_barvo1('green',S2))
+        S2 = tk.Button(master, text = "Spolzgad", relief='sunken',
+                       command = lambda: self.izberi_barvo('green',S2))
         S2.grid(row=4, column=6)
 
         self.gumbi_igralca2 = [G2, P2, D2, S2]
@@ -77,15 +77,21 @@ class Zacetno:
             pass
 
 
-    def izberi_barvo1(self, barva, gumb):
+    def izberi_barvo(self, barva, gumb):
+        
         if barva in self.dovoljene_barve:
             self.dovoljene_barve.remove(barva)
             if gumb in self.gumbi_igralca1:
                 self.dovoljene_barve.append(self.barva_igralec1)
                 self.barva_igralec1 = barva
+                for gumbek in self.gumbi_igralca1:
+                    gumbek.config(relief = 'raised')
             if gumb in self.gumbi_igralca2:
                 self.dovoljene_barve.append(self.barva_igralec2)
                 self.barva_igralec2 = barva
+                for gumbek in self.gumbi_igralca2:
+                    gumbek.config(relief = 'raised')
+            gumb.config(relief='sunken')
         else:
             pass
 
