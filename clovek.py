1717f9e45b44bc4467ea5637a77c4c902b1fc2d9
Nina Stempelj <nina.stempelj@gmail.com>
2017-05-02 18:41:36 +0200

    Dodani opisi funkcij.

--- clovek.py
+++ clovek.py
@@ -1,22 +1,23 @@
 
-class Clovek():
+class Clovek:
     
     def __init__(self, gui):
         self.gui = gui
 
     def povleci_potezo(self):
-        #to funkcijo rabi samo računalnik - kot pri Bauerju igraj
+        # to funkcijo rabi samo računalnik - kot pri Bauerju igraj
         pass
 
     def prekini(self):
         # To metodo kliče GUI, če je treba prekiniti razmišljanje.
-        # Človek jo lahko ignorira. Test
+        # Človek jo lahko ignorira
         pass
 
     def klik(self, novo):
-        # Povlečemo potezo. Če ni veljavna, se ne bo zgodilo nič.
+        """Povlečemo potezo. Če ni veljavna, se ne bo zgodilo nič."""
         self.gui.povleci_korak(novo)
 
     def povleci_korak(self):
+        # za računalnik je drugače, če vleče korak ali potezo, zato še ta.
+        # Človek v obeh primerih čaka na klik uporabnika.
         pass
-
--- racunalnik.py
+++ racunalnik.py
@@ -1,61 +1,70 @@
 import threading
-import time
-import minimax
+# import minimax
+
 
 class Racunalnik:
 
     def __init__(self, gui, algoritem):
         self.gui = gui
-        self.stevec_korakov = 0
-        self.naslednja_polja = []
+        self.stevec_korakov = 0  # števec doslej izrisanih korakov
+        self.naslednja_polja = []  # tu se bo shranila poteza
+        #  URŠA: a bi blo boljš, da to preimenujeva v poteza al pa kej tazga?
         self.algoritem = algoritem
         self.mislec = None
 
 
     def povleci_potezo(self):
-         self.stevec_korakov = 0
-         # kliče algoritem, nastavi self.poteze na naslednjo
-         self.mislec = threading.Thread(
-              target=lambda: self.algoritem.izracunaj_potezo(self.gui.igra.kopija()))
-         # Poženemo vlakno:
-         self.mislec.start()
-         # Gremo preverjat, ali je bila najdena poteza:
-         print("preverjamo v povleci_ potezo")
-         self.gui.polje.after(100, self.preveri_potezo)
-         #self.preveri_potezo()
+        """Požene algoritem na kopiji igre - požene vlakno"""
+        # Ko se začne poteza, ponastavimo števec korakov:
+        self.stevec_korakov = 0
+        # Ustvarimo vlakno:
+        self.mislec = \
+            threading.Thread(target=lambda: self.algoritem.izracunaj_potezo(self.gui.igra.kopija()))
+        # Poženemo vlakno:
+        self.mislec.start()
+        # Gremo preverjat, ali je bila najdena poteza:
+        ### print("preverjamo v povleci_ potezo")
+        self.gui.polje.after(100, self.preveri_potezo)
 
     def preveri_potezo(self):
         """Vsakih 100ms preveri, ali je algoritem že izračunal potezo."""
-        print("Računalnik preverja ali je že poteza")
+        ### print("Računalnik preverja ali je že poteza")
         if self.algoritem.poteza is not None:
-            print("pršu 1")
-            # Algoritem je našel potezo, povleci jo, če ni bilo prekinitve
+            ### print("pršu 1")
+            # Algoritem je našel potezo, shrani si jo, da jo mora narediti
             self.naslednja_polja = self.algoritem.poteza
             # Vzporedno vlakno ni več aktivno, zato ga "pozabimo"
-            print("Računalnik našel potezo: ", self.naslednja_polja)
+            ### print("Računalnik našel potezo: ", self.naslednja_polja)
             self.mislec = None
             self.povleci_korak()
+            # s tem klicem začnemo res izvajati potezo (po korakih)
         else:
             print("pršu2")
             # Algoritem še ni našel poteze, preveri še enkrat čez 100ms
             self.gui.polje.after(100, self.preveri_potezo)
 
+    def povleci_korak(self):
+        """Ko enkrat računalnik naračuna celo potezo,
+        jo tu narišemo po posameznih korakih."""
+
+        # Prvi element poteze je trenutni položaj,
+        # zato števec povečamo pred risanjem:
+        self.stevec_korakov += 1
+        ### print("polja za narisat", self.naslednja_polja, "števec korakov", self.stevec_korakov)
+        self.gui.polje.after(300, lambda: self.gui.povleci_korak(
+                                  self.naslednja_polja[self.stevec_korakov]))
+
+    def klik(self, novo):
+        # Računalnik ignorira klik
+        pass
+
+
     def prekini(self):
-        # To metodo kliče GUI, če je treba prekiniti razmišljanje.
-        # Človek jo lahko ignorira.
-         if self.mislec:
-            logging.debug ("Prekinjamo {0}".format(self.mislec))
+        """Če kdo zapre okno ali drugače nasilno prekine razmišljanje.
+        To metodo kliče GUI, če je treba prekiniti razmišljanje. """
+        if self.mislec:
             # Algoritmu sporočimo, da mora nehati z razmišljanjem
             self.algoritem.prekini()
             # Počakamo, da se vlakno ustavi
             self.mislec.join()
             self.mislec = None
-
-    def povleci_korak(self):
-        self.stevec_korakov += 1
-        print("polja za narisat", self.naslednja_polja, "števec korakov", self.stevec_korakov)
-        self.gui.polje.after(300, lambda: self.gui.povleci_korak(self.naslednja_polja[self.stevec_korakov]))
-
-    def klik(self, novo):
-        # Računalnik ignorira klik
-        pass
